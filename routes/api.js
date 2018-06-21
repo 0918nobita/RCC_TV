bdab1604badc5809cea122b08ba02ce3ddd9c2b6
0918nobita <nobita.0918@gmail.com>
2018-06-22 08:26:11 +0900

    BitbucketのLTVODリポジトリのsubtitilesフォルダ内にある、字幕ファイルの一覧を取得するようにした

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,26 @@
-const express = require('express'),
+const argv = require('argv'),
+      axios = require('axios'),
+      express = require('express'),
       router = express.Router();
 
-router.get('/', function(req, res, next) {
+const asyncMiddleware = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+router.get('/', asyncMiddleware(async (req, res, next) => {
+  const args = argv.run().targets,
+        auth = { auth: { username: args[0], password: args[1] } };
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(200);
-  res.send({
-    'api-version': '1.0'
+  axios.get('https://api.bitbucket.org/2.0/repositories/' + args[0] + '/ltvod/src', auth)
+  .then(result => result.data.values[0].links.self.href)
+  .then(href => {
+    axios.get(href, auth)
+    .then(result => {
+      const files = result.data.values.find(value =>
+        (new RegExp(/subtitles\/.*\.srt/)).test(value.path));
+      res.send(JSON.stringify(files));
+    });
   });
-});
+}));
 
 module.exports = router;
