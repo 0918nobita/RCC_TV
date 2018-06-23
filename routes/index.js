f2fa450537b50a312402dcf5039ea5db5941650c
0918nobita <nobita.0918@gmail.com>
2018-06-23 11:30:47 +0900

    function式ではなくアロー関数を使用するようにした

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(200);
   res.send(JSON.stringify("index"));
