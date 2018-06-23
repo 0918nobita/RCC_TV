ea1c670ca36d2a0bf97cb1236b156430744bee38
0918nobita <nobita.0918@gmail.com>
2018-06-23 10:05:28 +0900

    字幕ファイルの内容を取得して出力するようにした

--- routes/api.js
+++ routes/api.js
@@ -8,18 +8,24 @@ const asyncMiddleware = fn => (req, res, next) =>
 router.get('/', asyncMiddleware(async (req, res, next) => {
   const args = argv.run().targets,
         auth = { auth: { username: args[0], password: args[1] } };
+  
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(200);
-  axios.get('https://api.bitbucket.org/2.0/repositories/' + args[0] + '/ltvod/src', auth)
-  .then(result => result.data.values[0].links.self.href)
-  .then(href => {
-    axios.get(href, auth)
-    .then(result => {
-      const files = result.data.values;
-      console.log(files);
-      res.send(JSON.stringify(files));
+  
+  const result = await axios.get('https://api.bitbucket.org/2.0/repositories/' + args[0] + '/ltvod/src', auth);
+  const href = result.data.values[0].links.self.href;
+  const result2 = await axios.get(href, auth);
+  const files = result2.data.values;
+
+  for (let file of files) {
+    console.log('[' + file.path + ']');
+    
+    await axios.get(file.links.self.href, auth).then(result => {
+      console.log(result.data);
     });
-  });
+  }
+
+  res.send(JSON.stringify(files));
 }));
 
 module.exports = router;
