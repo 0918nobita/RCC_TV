f7ad8adc6af9185df41850ef9637fe01e607c4f0
0918nobita <nobita.0918@gmail.com>
2019-03-12 20:03:23 +0900

    :sparkles: サンプルの Saga を追加した

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import { reducer } from './store/_reducer';
 import PlayerComponent from './player';
 import CounterContainer from './containers/counterContainer';
+import { exampleSaga } from './sagas/example';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(exampleSaga);
 
 render(
   <Provider store={store}>
---
+++ workspaces/client/src/sagas/example.ts
@@ -0,0 +1,5 @@
+export function* exampleSaga() {
+  console.log('Hello, sagas!');
+  yield 'value';
+  return 0;
+}
