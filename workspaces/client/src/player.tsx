45cee10f48c962d0042abd44a273326a0bccbb6a
0918nobita <nobita.0918@gmail.com>
2019-03-12 18:28:41 +0900

    :construction: HLS 再生のサンプルを無効化した

--- workspaces/client/src/player.tsx
+++ workspaces/client/src/player.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
-import Hls from 'hls.js';
-
-console.log(Hls.isSupported());
+// import Hls from 'hls.js';
 
 type Props = {
   src: string;
 };
 
 export default class PlayerComponent extends React.Component<Props> {
-  private hls: Hls;
-  private player: HTMLVideoElement | null = null;
+  // private hls: Hls;
+  // private player: HTMLVideoElement | null = null;
 
   constructor(props: Props) {
     super(props);
-    this.hls = new Hls();
+    // this.hls = new Hls();
   }
 
+  // DOM 構築後
   componentDidMount() {
+    /*
     if (this.player === null) return;
 
     this.hls.attachMedia(this.player);
@@ -28,9 +28,13 @@ export default class PlayerComponent extends React.Component<Props> {
         if (this.player !== null) this.player.play();
       });
     });
+    */
   }
 
   render() {
-    return (<video ref={player => {this.player = player;}} preload="none" autoPlay={true}></video>);
+    return (
+      /*<video ref={player => {this.player = player;}} preload="none" autoPlay={true}></video>*/
+      <div></div>
+    );
   }
 }
