d80a1e70e8a2ea7e5c44aa33dd7ccc0c4135a223
0918nobita <nobita.0918@gmail.com>
2019-03-16 22:45:04 +0900

    :sparkles: VODContainer を追加した

---
+++ workspaces/client/src/containers/vodContainer.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Dispatch, bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { GlobalState } from '@/store/_state';
+import { VODState } from '@/store/vod/state';
+import { VODAction, actions, actionsType } from '@/store/vod/actions';
+
+interface Props {
+  state: VODState;
+  actions: actionsType;
+}
+
+class VODContainer extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (<div></div>);
+  }
+}
+
+export default connect(
+  (state: GlobalState) => ({ state: state.vod }),
+  (dispatch: Dispatch<VODAction>) => ({ actions: bindActionCreators(actions, dispatch) })
+)(VODContainer);
