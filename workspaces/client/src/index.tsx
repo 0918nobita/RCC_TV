8f1a9f449f2ce5b05eae0ae50d8d134e4c7413a2
0918nobita <nobita.0918@gmail.com>
2019-03-17 21:11:35 +0900

    :sparkles: redux-logger を用いてアクションのディスパッチ/ステート変更の内容をログ出力するようにした

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import { reducer } from './store/_reducer';
@@ -10,7 +11,7 @@ import rootSaga from '@/sagas/root';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store = createStore(reducer, applyMiddleware(sagaMiddleware, createLogger()));
 
 sagaMiddleware.run(rootSaga);
 
