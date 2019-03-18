2e732c409828a424b4e481ae0baa939ad9bf2d35
0918nobita <nobita.0918@gmail.com>
2019-03-19 00:45:24 +0900

    :bug: 「SSE 受信開始」ボタンを押す前にカウントアップが始まってしまう不具合を修正
    実際には起動時に通信が始まっていたため、EventSource のインスタンス化タイミングを変更して対応した

--- workspaces/client/src/sagas/sseTest.ts
+++ workspaces/client/src/sagas/sseTest.ts
@@ -4,17 +4,17 @@ import { Observable } from 'rxjs';
 
 import { LiveActionTypes, onMessage, fail } from '@/store/live/actions';
 
-const sseChannel = eventChannel<string>(emit => {
-  const receive$ = new Observable<string>(observer => {
-    const evtSource = new EventSource('http://localhost:8080/live');
-    evtSource.onmessage = x => observer.next(x.data);
-    return () => evtSource.close();
-  });
-  return receive$.subscribe(x => emit(x)).unsubscribe;
-});
-
 export function* sseTest() {
   yield take(LiveActionTypes.Start);
+
+  const sseChannel = eventChannel<string>(emit => {
+    const receive$ = new Observable<string>(observer => {
+      const evtSource = new EventSource('http://localhost:8080/live');
+      evtSource.onmessage = x => observer.next(x.data);
+      return () => evtSource.close();
+    });
+    return receive$.subscribe(x => emit(x)).unsubscribe;
+  });
 
   while (true) {
     const count: number = yield take(sseChannel);
