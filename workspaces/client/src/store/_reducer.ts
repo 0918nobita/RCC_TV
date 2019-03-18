a8010a965f4749ff8c80308e5d62b6190ad285f6
0918nobita <nobita.0918@gmail.com>
2019-03-18 21:39:57 +0900

    :wrench: LiveStore を利用できるようにした

--- workspaces/client/src/store/_reducer.ts
+++ workspaces/client/src/store/_reducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as vodReducer } from './vod/reducer';
+import { reducer as liveReducer } from './live/reducer';
 
 export const reducer = combineReducers({
   vod: vodReducer,
+  live: liveReducer
 });
--- workspaces/client/src/store/_state.ts
+++ workspaces/client/src/store/_state.ts
@@ -1,5 +1,7 @@
 import { VODState } from './vod/state';
+import { LiveState } from './live/state';
 
 export interface GlobalState {
   vod: VODState;
+  live: LiveState;
 }
