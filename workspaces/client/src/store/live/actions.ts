e9e092d21021139e53431ad12919d4c41ef3e82b
0918nobita <nobita.0918@gmail.com>
2019-03-18 21:39:16 +0900

    :construction: LiveStore の各アクションを定義した

---
+++ workspaces/client/src/store/live/actions.ts
@@ -0,0 +1,31 @@
+export enum LiveActionTypes {
+  Start = 'live/start SSE test',
+  OnMessage = 'live/onMessage',
+  Fail = 'live/fail',
+}
+
+type Start = { type: LiveActionTypes.Start };
+
+export const start = (): Start => ({
+  type: LiveActionTypes.Start,
+});
+
+type OnMessage = { type: LiveActionTypes.OnMessage; payload: { count: number } }
+
+export const onMessage = (count: number): OnMessage => ({
+  type: LiveActionTypes.OnMessage,
+  payload: { count }
+});
+
+type Fail = { type: LiveActionTypes.Fail; payload: { message: string } };
+
+export const fail = (message: string): Fail => ({
+  type: LiveActionTypes.Fail,
+  payload: { message },
+});
+
+export type VODAction = Start | OnMessage | Fail;
+
+export const actions = { start, onMessage, fail };
+
+export type actionsType = typeof actions;
