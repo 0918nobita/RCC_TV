b5a8a7d2f2fdd3957b303ff7d6f4a2bd2e2b17d4
0918nobita <nobita.0918@gmail.com>
2019-03-20 13:49:08 +0900

    :card_file_box: videos テーブルを生成しサンプルのレコードを挿入する SQL を追加した

---
+++ workspaces/rds/init/3_init_videos.sql
@@ -0,0 +1,12 @@
+CREATE TABLE rcctv.videos (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title varchar(50) NOT NULL,
+  description TEXT,
+  url TEXT,
+  thumbnail TEXT
+);
+
+INSERT INTO rcctv.videos (title, description, url) VALUES
+  ('テスト1', '説明文1', 'http://localhost:3000/video.m3u8'),
+  ('テスト2', '説明文2', 'http://localhost:3000/video.m3u8'),
+  ('テスト3', '発表者が 2 名の場合のテスト', 'http://localhost:3000/video.m3u8');
