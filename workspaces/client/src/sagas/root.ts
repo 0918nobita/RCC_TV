455133c60465320757c0961135b0fb799218620d
0918nobita <nobita.0918@gmail.com>
2019-04-04 10:48:50 +0900

    :white_check_mark: 動画メタデータを取得するサンプルを更新した
    実際の動画 ID を指定するようにした

--- workspaces/client/src/sagas/root.ts
+++ workspaces/client/src/sagas/root.ts
@@ -3,6 +3,6 @@ import { getVideoMetadata } from './getVideoMetadata';
 import { sseTest } from './sseTest';
 
 export default function* rootSaga() {
-  yield fork(getVideoMetadata, 'Th1s1sV1De0iD');
+  yield fork(getVideoMetadata, '1');
   yield fork(sseTest);
 }
