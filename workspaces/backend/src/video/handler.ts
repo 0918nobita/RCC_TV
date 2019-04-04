543daf9773eb55842d77ec59e45dd1b51039771c
0918nobita <nobita.0918@gmail.com>
2019-04-04 10:48:04 +0900

    :sparkles: /video/:videoId に対する動画メタデータの取得処理を実装した

--- workspaces/backend/src/video/handler.ts
+++ workspaces/backend/src/video/handler.ts
@@ -1,10 +1,36 @@
 import { Request, Response } from 'express';
+import MySQL from 'mysql';
 
-export const handler = (_: Request, res: Response) => {
-  return res.json({
-    id: 'Th1s1sV1De0iD',
-    url: 'http://localhost:3000/video.m3u8',
-    title: 'サンプルのビデオ',
-    desc: '説明文',
-  });
+import { dbConfig } from '../config';
+import { connect, query } from '../utils/sql';
+import { VideosRecord } from '../models/records';
+
+export const handler = async (req: Request, res: Response) => {
+  const videoId: string = req.params.videoId;
+
+  const connection = MySQL.createConnection(dbConfig);
+
+  try {
+    await connect(connection);
+    const videos = await query<VideosRecord>(
+      connection,
+      'SELECT * FROM videos WHERE id=?',
+      [videoId]
+    );
+    if (videos.length === 0) {
+      console.log(
+        `Video ID '${videoId}' に対する動画リソースが見つかりませんでした`
+      );
+      res.writeHead(400);
+      res.end();
+      return;
+    }
+    res.json(videos[0]);
+  } catch (e) {
+    console.log(e);
+    res.writeHead(500);
+    res.end();
+  } finally {
+    connection.end();
+  }
 };
