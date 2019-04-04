c8199bb19d264c6a69eb2cbd3486df8081867296
0918nobita <nobita.0918@gmail.com>
2019-04-04 14:35:11 +0900

    :building_construction: /account/:name でのユーザーが見つからない場合の例外処理を追加した

--- workspaces/backend/src/account/handler.ts
+++ workspaces/backend/src/account/handler.ts
@@ -17,6 +17,14 @@ export const handler = async (req: Request, res: Response) => {
       `SELECT * FROM users WHERE name=?`,
       [name]
     );
+    if (users.length === 0) {
+      console.log(
+        `ユーザーネーム '${name}' に対するアカウントが見つかりませんでした`
+      );
+      res.writeHead(400);
+      res.end();
+      return;
+    }
     res.json(users[0]);
   } catch (e) {
     console.log(e);
