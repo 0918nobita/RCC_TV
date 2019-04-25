01bd3acd0c624b94e9204e989631c8057e88f737
0918nobita <nobita.0918@gmail.com>
2019-04-25 20:57:09 +0900

    :sparkles: /account/~ API 向けの Router を追加した

---
+++ workspaces/backend/src/account/router.ts
@@ -0,0 +1,47 @@
+import { Router, Request, Response } from 'express';
+import MySQL from 'mysql';
+import moment from 'moment';
+
+import { UsersRecord } from '../models/records';
+import { connect, query } from '../utils/sql';
+import { dbConfig } from '../config';
+
+export const router = Router();
+
+// endpoint prefix: /account
+
+router.get('/:name', async (req: Request, res: Response) => {
+  const name: string = req.params.name;
+
+  const connection = MySQL.createConnection(dbConfig);
+
+  try {
+    await connect(connection);
+    const users = await query<UsersRecord>(
+      connection,
+      `SELECT * FROM users WHERE name=?`,
+      [name]
+    );
+    if (users.length === 0) {
+      console.log(
+        `ユーザーネーム '${name}' に対するアカウントが見つかりませんでした`
+      );
+      res.writeHead(404);
+      res.end();
+      return;
+    }
+    const account = users[0];
+    res.json(
+      Object.assign(account, {
+        created: moment(account.created).format('YYYY年M月D日 H:m'),
+        modified: moment(account.modified).format('YYYY年M月D日 H:m'),
+      })
+    );
+  } catch (e) {
+    console.log(e);
+    res.writeHead(500);
+    res.end();
+  } finally {
+    connection.end();
+  }
+});
