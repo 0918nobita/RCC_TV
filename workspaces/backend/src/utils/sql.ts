056e1580e9ec5b5c55d727ac7f1b108aa23569da
Yugo Yagita <2000ygkn0713@gmail.com>
2019-05-03 09:20:33 +0900

    :heavy_plus_sign:  Add Params type

--- workspaces/backend/src/utils/sql.ts
+++ workspaces/backend/src/utils/sql.ts
@@ -1,5 +1,16 @@
 import MySQL from 'mysql';
 
+export type Params<T> =
+  | number
+  | boolean
+  | Date
+  | Buffer
+  | string
+  | { [K in keyof T]: T[keyof T] }
+  | { toSqlString(): string; [key: string]: any }
+  | undefined
+  | null;
+
 export const connect = (connection: MySQL.Connection) =>
   new Promise<void>((resolve: () => void, reject) => {
     connection.connect(err => {
